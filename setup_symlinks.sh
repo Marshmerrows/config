e1d18f89dd00815913b7521830350e6e0453498c
Jerry Li <jerry@skusafe.com>
2025-06-30 00:53:26 -0400

    split zshrc'

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -4,8 +4,10 @@ CONFIG_DIR="$(pwd)"
 
 ln -sfn "${CONFIG_DIR}/hammerspoon" ~/.hammerspoon
 ln -sfn "${CONFIG_DIR}/vim/vimrc" ~/.vimrc
-ln -sfn "${CONFIG_DIR}/zsh/zshrc" ~/.zshrc
 ln -sfn "${CONFIG_DIR}/zsh/zsh_plugins.txt" ~/.zsh_plugins.txt
+
+# Create ~/.zshrc that sources the shared config
+echo "source \"${CONFIG_DIR}/zsh/zshrc\"" > ~/.zshrc
 
 echo "Symlinks created successfully:"
 ls -l ~/.hammerspoon ~/.vimrc ~/.zshrc ~/.zsh_plugins.txt
