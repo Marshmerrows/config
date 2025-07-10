02b35dee0512e66ac3209884bb64d7325b2b2750
Jerry Li <jerry@skusafe.com>
2025-07-10 15:53:18 -0400

    start of consolidation

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,71 @@
--- Load core configuration
-require("core.options")
-require("core.keymaps")
-require("core.autocmds")
+vim.g.mapleader = " "
+
+-- See `:help vim.o`
+--  For more options, you can see `:help option-list`
+vim.g.have_nerd_font = true
+vim.o.number = true
+vim.o.mouse = "a"
+vim.o.showmode = false
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = "auto:1"
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.autoread = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.o.inccommand = "split"
+vim.o.cursorline = true
+vim.o.scrolloff = 3
+vim.o.tabstop = 2 -- Number of spaces a tab character displays as
+vim.o.softtabstop = 2 -- Number of spaces for <Tab> in insert mode
+vim.o.shiftwidth = 2 -- Number of spaces for autoindent (e.g. when you press Enter)
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- Clear highlights on search when pressing <Esc> in normal mode
+
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" }) -- Diagnostic keymaps
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Map ii to escape in insert mode
+vim.keymap.set("i", "ii", "<Esc>", { desc = "Exit insert mode" })
+
+--  Use CTRL+<hjkl> to switch between windows
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
