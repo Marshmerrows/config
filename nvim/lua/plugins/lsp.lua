479aeb9b54fea668ad39c6af1664f4f0bda869fc
Jerry Li <jerry@skusafe.com>
2025-08-05 22:35:03 -0400

    bleh

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -188,9 +188,22 @@ return {
 			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+
+			local lspconfig_util = require("lspconfig.util")
+
 			local servers = {
 				basedpyright = {},
 				ts_ls = {},
+				svelte = {
+					settings = {
+						svelte = {
+							plugin = {
+								typescript = { enable = true },
+								css = { enable = true },
+							},
+						},
+					},
+				},
 				lua_ls = {
 					settings = {
 						Lua = {
@@ -200,7 +213,6 @@ return {
 						},
 					},
 				},
-				svelte = {},
 				html = {},
 				cssls = {},
 				jsonls = {},
