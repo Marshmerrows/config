c0162108c647e1d103669fef48672a3fea756b80
Jerry Li <jerryl3698@gmail.com>
2025-09-02 09:37:46 -0400

    oil and some other updates

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -59,36 +59,24 @@ return {
 
 					-- Find references for the word under your cursor.
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-					map("grr", "<cmd>Trouble lsp_references toggle<cr>", "[G]oto [R]eferences (Trouble)")
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
 					map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-					map("gii", "<cmd>Trouble lsp_implementations toggle<cr>", "[G]oto [I]mplementations (Trouble)")
 
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-					map("gdd", "<cmd>Trouble lsp_definitions toggle<cr>", "[G]oto [D]efinitions (Trouble)")
 
-					-- WARN: This is not Goto Definition, this is Goto Declaration.
+					-- This is not Goto Definition, this is Goto Declaration.
 					--  For example, in C this would take you to the header.
-					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
 					map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
-					map("gtt", "<cmd>Trouble lsp_type_definitions toggle<cr>", "[G]oto [T]ype Definitions (Trouble)")
-
-					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
-					map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
-
-					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
-					map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
 
 					-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
 					---@param client vim.lsp.Client
@@ -173,6 +161,7 @@ return {
 				virtual_text = {
 					source = "if_many",
 					spacing = 2,
+					wrap = true,
 					format = function(diagnostic)
 						local diagnostic_message = {
 							[vim.diagnostic.severity.ERROR] = diagnostic.message,
